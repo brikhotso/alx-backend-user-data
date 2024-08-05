1a23f86c521b7f2347cb452c3fd9b8b829dae096
brikhotso <brenda.rikhotso@gmail.com>
2024-08-05 23:10:08 +0200

    pycodestyle requirements

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,7 +27,6 @@ def unauthorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
-
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Request forbidden handler"""
