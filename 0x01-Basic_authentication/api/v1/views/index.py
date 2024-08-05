e1929b32711b1a5922ae0fc16e9140d6f31fdae0
brikhotso <brenda.rikhotso@gmail.com>
2024-08-06 00:21:10 +0200

    pycode requirements

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -28,7 +28,7 @@ def stats() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized_test():
-    """ 
+    """
     GET /api/v1/unauthorised
     """
     abort(401)
