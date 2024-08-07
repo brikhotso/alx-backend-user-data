e496eb8efbd0cb93a2e47651815e1d8938717882
brikhotso <brenda.rikhotso@gmail.com>
2024-08-07 22:34:39 +0200

    return a cookie value from a request

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" Contain class to manage the API authentication"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Manage the API authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Determines if authentication is required for a given path"""
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        for ex_path in excluded_paths:
+            if path == ex_path or path.startswith(ex_path[:-1]):
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Retrieves the authorization header from the request"""
+        if request is None:
+            return None
+
+        if 'Authorization' not in request.headers:
+            return None
+
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the current user from the request."""
+        return None
+
+    def session_cookie(self, request=None):
+        """Return a cookie value from a request"""
+        if request is None:
+            return None
+
+        session_name = os.environ.get("SESSION_NAME", "_my_session_id")
+        return request.cookies.get(session_name)
